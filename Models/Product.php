809fe6db3b14ed91f1def954b39d97445d22f29d
Luis Alejandro Ramirez <lramirezsuarez@gmail.com>
2017-05-09 13:00:14 -0500

    #6 -Remove some commented lines, corrected a grammar mistake.

--- Models/Product.php
+++ Models/Product.php
@@ -181,13 +181,12 @@ class Product {
             if($statement->num_rows > 0){
                 while($log = $statement->fetch_assoc()){
                     echo "<tr><form action='../Views/Opcion2.php' method='post'>";
-                    echo "<td><input type='number' name='proID' value='".$log['productID']."' readonly /></td>";
+                    echo "<td><input type='text' name='proID' value='".$log['productID']."' readonly /></td>";
                     echo "<td><input type='text' name='proName' value='".$log['name']."' readonly /></td>";
                     echo "<td><input type='text' name='proDes' value='".$log['description']."' readonly /></td>";
-                    echo "<td><input type='number' name='proQuan' value='".$log['quantity']."' readonly /></td>";
-                    echo "<td><input type='number' name='proCost' value='".$log['cost']."' readonly /></td>";
+                    echo "<td><input type='text' name='proQuan' value='".$log['quantity']."' readonly /></td>";
+                    echo "<td><input type='text' name='proCost' value='".$log['cost']."' readonly /></td>";
                     echo "<td><input type='text' name='proCateg' value='".$log['categID']."' readonly /></td>";
-                    //echo "<td><input  type='submit' class='btn btn-warning' value='Edit' data-toggle='tooltip' title='Editar'/></td>";
                     echo "<td><button type='submit' class='glyphicon glyphicon-edit btn btn-warning' data-toggle='tooltip' title='Edit' /></td>";
                     echo "<td><button type='submit' class='glyphicon glyphicon-trash btn btn-danger' data-toggle='tooltip' title='Delete' formaction='../Controllers/DeleteController.php' /></td>";
                     echo "</form></tr>";
